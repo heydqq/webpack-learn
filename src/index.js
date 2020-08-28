f6ea6388ec0ca3ca8a7637746b58c6606b3309c7
douqinqin <douqinqin@58.com>
2020-08-28 14:52:23 +0800

    feat:dynamic import

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import _ from 'lodash';
-import Json from './assets/data.json';
-// import printMe from './print';
-// import './assets/style.css';
-// import Food from './assets/food.png'
-// import xmlData from './assets/xmlData.xml';
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], Json.hello);
-    element.classList.add('hello')
-    //引入各种assets
-    // const myFood = new Image();
-    // myFood.src=Food;
-    // element.appendChild(myFood)
-    // console.log(xmlData);
-    return element;
+  function getComponent(){
+    return import(/*webpackChunkName:'lodash' */ 'lodash').then(({default:_}) => {
+      const element = document.createElement('div');
+      element.innerHTML = _.join(['Hello', 'webpack'], '');
+      element.classList.add('hello')
+      return element;
+    }).catch(err => `An error occurred while loading the component${err}`)
   }
-  
-  document.body.appendChild(component());
+
+  getComponent().then(component => {
+    document.body.appendChild(component)
+  })
--- webpack.dqq.js
+++ webpack.dqq.js
@@ -4,18 +4,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
-    // chunkFilename:'[name].bundle.js',
+    chunkFilename:'[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
- optimization:{
-  splitChunks:{
-    chunks:'all'
-  }
- },
   devtool: 'inline-source-map',
   devServer:{
     contentBase:'./dist',
