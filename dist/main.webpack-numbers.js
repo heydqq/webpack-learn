e7f4d1f367331fbcff906b76c8a4086330556885
douqinqin <douqinqin@58.com>
2020-08-31 16:08:15 +0800

    feaf:webpach library

---
+++ dist/main.webpack-numbers.js
@@ -0,0 +1,58 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! exports provided: numToWord, wordToNum */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "numToWord", function() { return numToWord; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "wordToNum", function() { return wordToNum; });
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "lodash");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _ref_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ref.json */ "./src/ref.json");
+var _ref_json__WEBPACK_IMPORTED_MODULE_1___namespace = /*#__PURE__*/__webpack_require__.t(/*! ./ref.json */ "./src/ref.json", 1);
+
+
+
+function numToWord(num) {
+  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
+}
+
+function wordToNum(word) {
+  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
+
+/***/ }),
+
+/***/ "./src/ref.json":
+/*!**********************!*\
+  !*** ./src/ref.json ***!
+  \**********************/
+/*! exports provided: 0, 1, 2, 3, 4, 5, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"num\":1,\"word\":\"One\"},{\"num\":2,\"word\":\"Two\"},{\"num\":3,\"word\":\"Three\"},{\"num\":4,\"word\":\"Four\"},{\"num\":5,\"word\":\"Five\"},{\"num\":0,\"word\":\"Zero\"}]");
+
+/***/ }),
+
+/***/ "lodash":
+/*!*************************************************************************************!*\
+  !*** external {"commonjs":"lodash","commonjs2":"lodash","amd":"lodash","root":"_"} ***!
+  \*************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = undefined;
+
+/***/ })
+
+},[["./src/index.js","runtime~main"]]]);
+//# sourceMappingURL=main.webpack-numbers.js.map
--- webpack.dqq.js
+++ webpack.dqq.js
@@ -1,57 +1,24 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
+
+module.exports =  [
+  'source-map'
+].map(devtool => ({
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js'
   },
-  optimization:{
-    moduleIds:'hashed',
-    runtimeChunk:'single',
-    splitChunks: {
-      cacheGroups:{
-        vendor:{
-          test:/[\\/]node_modules[\\/]/,
-          name:'vendors',
-          chunks:'all'
-        }
-      },
-    },
+  externals:{
+    lodash:{
+      commonjs:'lodash',
+      commonjs2:'lodash',
+      amd:'lodash',
+      root:'_'
+    }
   },
-  devtool: 'inline-source-map',
-  module:{
-    rules:[
-      {
-        test:/\.css$/,
-        use:[
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test:/\.(png|svg|jpg|gif)$/,
-        use:[
-          'file-loader',
-        ]
-      },
-      {
-        test:/\.xml$/,
-        use:[
-          'xml-loader',
-        ]
-      },
-    ],
-  },
-  plugins:[
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets:false
-    }),
-    new HtmlWebpackPlugin({
-      title:'Output Mangament'
-    }),
-  ]
-};
+  devtool,
+  optimization: {
+    runtimeChunk: true
+  }
+}));
